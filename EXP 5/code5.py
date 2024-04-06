fdeae680f9eac0da9863dda7e6cd375ba34dc5fb
Tanmay-Dalvi <tanmaydalvi1223@gmail.com>
2024-04-06 22:11:35 +0530

    changes

--- EXP 5/code5.py
+++ EXP 5/code5.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('salary_data.csv')
 dataset.head()
 
 # data preprocessing
